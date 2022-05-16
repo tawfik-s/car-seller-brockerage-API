95b3469d0378a52522a0a392f803f32bd51e0c84
tawfik shalash <t.shalash1@gmail.com>
2022-05-16 21:04:22 +0200

    add delete and update methods

--- Routes/car.js
+++ Routes/car.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { getCars, CreateCar } = require("../services/carService")
+const { getCars, CreateCar, DeleteCar, UpdateCar } = require("../services/carService")
 
 
 //get all data
 router.get("/", getCars);
 router.post("/", CreateCar);
+router.delete("/:id", DeleteCar);
+router.put("/:id", UpdateCar)
 
 
 
--- services/carService.js
+++ services/carService.js
@@ -30,5 +30,34 @@ exports.CreateCar = async (req, res) => {
         console.error(err.message);
         res.status(400).send({ message: 'error!' });
     }
+}
 
+
+exports.DeleteCar = async (req, res) => {
+    try {
+        const result = await Car.deleteOne({ _id: req.params.id });   //deleteMany to delete more than one
+        console.log(result);
+        res.status(200).send({ message: 'done' });
+    } catch (err) {
+        console.error(err.message);
+        res.status(400).send({ message: 'error!' });
+    }
+}
+
+exports.UpdateCar = async (req, res) => {
+    try {
+        const car = await Car.findById(req.params.id);
+        if (!car) res.send({ message: 'there is no such item' });
+        car.Car_name = req.body.Car_name;
+        car.Car_type = req.body.Car_type;
+        car.Car_color = req.body.Car_color;
+        car.tags = req.body.tags;
+        car.description = req.body.description;
+        const result = await car.save();
+        console.log(result);
+        res.status(200).send({ message: 'done' });
+    } catch (err) {
+        console.error(err.message);
+        res.status(400).send({ message: 'error!' });
+    }
 }
