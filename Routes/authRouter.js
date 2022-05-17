67e377d56a44490f4f5cf6773b228b2d47382a69
tawfik shalash <t.shalash1@gmail.com>
2022-05-17 18:42:13 +0200

    auth

--- Routes/authRouter.js
+++ Routes/authRouter.js
@@ -1,8 +1,39 @@
 const express = require('express');
 const router = express.Router();
 const { } = require("../services/authService")
+const jwt = require('jsonwebtoken')
 
+let refreshTokens = []
 
+router.post('/token', (req, res) => {      //refreshtoken
+    const refreshToken = req.body.token
+    if (refreshToken == null) return res.sendStatus(401)
+    if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403)
+        const accessToken = generateAccessToken({ name: user.name })
+        res.json({ accessToken: accessToken })
+    })
+})
+
+router.delete('/logout', (req, res) => {           //delete token
+    refreshTokens = refreshTokens.filter(token => token !== req.body.token)
+    res.sendStatus(204)
+})
+
+router.post('/login', (req, res) => {  //send access token
+    const username = req.body.username
+    const user = { name: username }
+
+    const accessToken = generateAccessToken(user)
+    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
+    refreshTokens.push(refreshToken)
+    res.json({ accessToken: accessToken, refreshToken: refreshToken })
+})
+
+function generateAccessToken(user) {
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1200s' })
+}
 
 
 module.exports = router;
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const carRouter = require('./Routes/carRouter');
 const authRouter = require('./Routes/authRouter')
 //const seller = require('./Routes/seller');
 const logger = require('./middleware/logger')
-
+require('dotenv').config()
 //my-middleware
 app.use(cors());
 app.use(express.json());
@@ -18,7 +18,7 @@ app.use('/auth', authRouter);
 
 
 //port
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log("server is running on port 3000")
 });
