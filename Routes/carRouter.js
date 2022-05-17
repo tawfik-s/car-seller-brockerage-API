4d9f08c7d5840a85457051db84e979c944c54421
tawfik shalash <t.shalash1@gmail.com>
2022-05-17 19:08:58 +0200

    add auth middleware and add seller Service

--- Routes/carRouter.js
+++ Routes/carRouter.js
@@ -7,12 +7,14 @@ const { getCars,
     getCarById
 } = require("../services/carService")
 
+const { authenticateToken } = require("../services/authService");
+
 
 router.get("/", getCars);
 router.get("/:id", getCarById);
-router.post("/", CreateCar);
-router.delete("/:id", DeleteCar);
-router.put("/:id", UpdateCar)
+router.post("/", authenticateToken, CreateCar);
+router.delete("/:id", authenticateToken, DeleteCar);
+router.put("/:id", authenticateToken, UpdateCar)
 
 
 
